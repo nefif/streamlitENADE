cb841f7a662cffecd9f862620e2427a2d3101606
Néfi Fernandes <nefifernandes14@yahoo.com.br>
2023-06-05 01:44:34 -0300

    Ajustes no código
    *remoção do mapeamento para diminuir a quantidade de linhas no código
    *Importação do novo dataset contendo as informações compiladas e mapeadas dos anos de 2018, 2019 e 2021;
    *Criado um novo filtro no dataset, para trazer apenas informações do ano de 2021;
    *Mudança na exibição do dados dos gráficos, facilitando o entendimento. Os dados agora são exibidos do maior valor para o menor valor.

--- teste.py
+++ teste.py
@@ -2,125 +2,19 @@ import pandas as pd
 import streamlit as st
 import altair as at
 
-df_2021 = pd.read_csv('datasets\microdados2021_arq1.txt', delimiter=';')
-df_2019 = pd.read_csv('datasets\microdados2019_arq1.txt', delimiter=';')
-df_2018 = pd.read_csv('datasets\microdados2018_arq1.txt', delimiter=';')
-df = pd.concat([df_2021, df_2019, df_2018], axis=0)
+df = pd.read_csv('datasets\incriçõesEnade.csv')
 
-mapeamento = {
-    'CO_REGIAO_CURSO': {
-        1: "Região Norte",
-        2: "Região Nordeste",
-        3: "Região Sudeste",
-        4: "Região Sul",
-        5: "Região Centro-Oeste"
-    },
-    'CO_UF_CURSO': {
-        11: "Rondônia (RO)",
-        12: "Acre (AC)",
-        13: "Amazonas (AM)",
-        14: "Roraima (RR)",
-        15: "Pará (PA)",
-        16: "Amapa (AP)",
-        17: 'Tocantins (TO)',
-        21: 'Maranhão (MA)',
-        22: 'Piauí (PI)',
-        23: 'Ceará (CE)',
-        24: 'Rio Grande do Norte (RN)',
-        25: 'Paraíba (PB)',
-        26: 'Pernambuco (PE)',
-        27: 'Alagoas (AL)',
-        28: 'Sergipe (SE)',
-        29: 'Bahia (BA)',
-        31: 'Minas gerais (MG)',
-        32: 'Espírito Santo (ES)',
-        33: 'Rio de Janeiro (RJ)',
-        35: 'São Paulo (SP)',
-        41: 'Paraná (PR)',
-        42: 'Santa Catarina (SC)',
-        43: 'Rio Grande do Sul (RS)',
-        50: 'Mato Grosso do Sul (MS)',
-        51: 'Mato Grosso (MT)',
-        52: 'Goiás (GO)',
-        53: 'Distrito federal (DF)',
-    },
-    'CO_CATEGAD': {
-        1: "Pública Federal",
-        2: "Pública Estadual",
-        3: "Pública Municipal",
-        4: "Privada com fins lucrativos",
-        5: "Privada sem fins lucrativos",
-        7: "Especial",
-    },
-    'CO_ORGACAD': {
-        10019: "Centro Federal de Educação Tecnológica",
-        10020: "Centro Universitário",
-        10022: "Faculdade ",
-        10026: "Instituto Federal de Educação, Ciência e Tecnologia",
-        10028: "Universidade",
-    },
-    'CO_MODALIDADE': {
-        0: "EaD",
-        1: "Presencial",
-    },
-    'CO_GRUPO': {
-        26: "Design",
-        72: "Tecnologia em Análise e Desenvolvimento de Sistemas",
-        79: "Tecnologia em Redes de Computadores",
-        702: "Matemática (Licenciatura)",
-        904: "Letras-Português (Licenciatura)",
-        905: "Letras-Português e Inglês (Licenciatura)",
-        906: "Letras-Português e Espanhol (Licenciatura)",
-        1402: "Física (Licenciatura)",
-        1501: "Química (Bacharelado)",
-        1502: "Química (Licenciatura)",
-        1601: "Ciências Biológicas (Bacharelado)",
-        1602: "Ciências Biológicas (Licenciatura)",
-        2001: "Pedagogia (Licenciatura)",
-        2401: "História (Bacharelado)",
-        2402: "História (Licenciatura)",
-        2501: "Artes Visuais (Licenciatura)",
-        3001: "Geografia (Bacharelado)",
-        3002: "Geografia (Licenciatura)",
-        3201: "Filosofia (Bacharelado)",
-        3202: "Filosofia (Licenciatura)",
-        3501: "Educação Física (Bacharelado)",
-        3502: "Educação Física (Licenciatura)",
-        4004: "Ciência Da Computação (Bacharelado)",
-        4005: "Ciência Da Computação (Licenciatura)",
-        4006: "Sistemas de Informação",
-        4301: "Música (Licenciatura)",
-        5401: "Ciências Sociais (Bacharelado)",
-        5402: "Ciências Sociais (Licenciatura)",
-        6407: "Letras-Inglês (Licenciatura)",
-        6409: "Tecnologia em Gestão da Tecnologia da Informação",
-    }
-}
-
-df_2021 = df_2021.replace(mapeamento)
-df_2019 = df_2019.replace(mapeamento)
-df = df.replace(mapeamento)
-
-filtro_2021 = df_2021[(df_2021['CO_CATEGAD'] == "Pública Federal") |
-                      (df_2021['CO_CATEGAD'] == "Pública Estadual") |
-                      (df_2021['CO_CATEGAD'] == "Pública Municipal")]
-
-filtro_2019 = df_2019[(df_2019['CO_CATEGAD'] == "Pública Federal") |
-                      (df_2019['CO_CATEGAD'] == "Pública Estadual") |
-                      (df_2019['CO_CATEGAD'] == "Pública Municipal")]
-
-filtro = df[(df['CO_CATEGAD'] == "Pública Federal") |
-            (df['CO_CATEGAD'] == "Pública Estadual") |
-            (df['CO_CATEGAD'] == "Pública Municipal")]
+filtro_2021 = df[df['NU_ANO'] == 2021]
 
 #Gráfico de Barras por Quantidade por Ano
-count_ano = filtro['NU_ANO'].value_counts()
+count_ano = df['NU_ANO'].value_counts()
 count_ano = pd.DataFrame(count_ano).reset_index()
 count_ano.index = range(1, len(count_ano) + 1)
 count_ano.columns = ['Ano do ENADE', 'Total de Inscrições']
 grafico_ano_barra = at.Chart(count_ano).mark_bar().encode(
     x=at.X('Ano do ENADE:N', axis=at.Axis(title='Ano do Enade')),
-    y=at.Y('Total de Inscrições:Q', axis=at.Axis(title='Total de Inscrições')))
+    y=at.Y('Total de Inscrições:Q', axis=at.Axis(title='Total de Inscrições')),
+    text='Total de Inscrições:Q')
 grafico_ano_linha = at.Chart(count_ano).mark_line(color='red').encode(
     x=at.X('Ano do ENADE:N', axis=at.Axis(title='Ano do Enade')),
     y=at.Y('Total de Inscrições:Q', axis=at.Axis(title='Total de Inscrições')))
@@ -133,7 +27,10 @@ count_regiao.columns = ['Região', 'Quantidade de Inscrições']
 count_regiao = count_regiao.sort_values(by='Quantidade de Inscrições',
                                         ascending=False)
 grafico_regiao = at.Chart(count_regiao).mark_bar().encode(
-    x='Região', y='Quantidade de Inscrições')
+    x=at.X('Região:N',
+           sort=at.SortField(field='Quantidade de Inscrições',
+                             order='descending')),
+    y='Quantidade de Inscrições:Q')
 
 #Gráfico de Barras por Estado do Brasil
 count_uf = filtro_2021['CO_UF_CURSO'].value_counts()
@@ -141,8 +38,11 @@ count_uf = pd.DataFrame(count_uf).reset_index()
 count_uf.index = range(1, len(count_uf) + 1)
 count_uf.columns = ['UF', 'Quantidade de Inscrições']
 count_uf = count_uf.sort_values(by='Quantidade de Inscrições', ascending=False)
-grafico_uf = at.Chart(count_uf).mark_bar().encode(x='UF',
-                                                  y='Quantidade de Inscrições')
+grafico_uf = at.Chart(count_uf).mark_bar().encode(
+    x=at.X('UF:N',
+           sort=at.SortField(field='Quantidade de Inscrições',
+                             order='descending')),
+    y='Quantidade de Inscrições:Q')
 
 #Gráfico de Barras por Categoria de Universidade
 count_categoria = filtro_2021['CO_CATEGAD'].value_counts()
@@ -154,7 +54,10 @@ count_categoria.columns = [
 count_categoria = count_categoria.sort_values(by='Quantidade de Inscrições',
                                               ascending=False)
 grafico_categoria = at.Chart(count_categoria).mark_bar().encode(
-    x='Categoria de Universidade', y='Quantidade de Inscrições')
+    x=at.X('Categoria de Universidade:N',
+           sort=at.SortField(field='Quantidade de Inscrições',
+                             order='descending')),
+    y='Quantidade de Inscrições:Q')
 
 #Gráfico de Barras por Grupo de Cursos de Gradução
 count_grupo_curso = filtro_2021['CO_GRUPO'].value_counts()
@@ -166,7 +69,10 @@ count_grupo_curso.columns = [
 count_grupo_curso = count_grupo_curso.sort_values(
     by='Quantidade de Inscrições', ascending=False)
 grafico_grupo = at.Chart(count_grupo_curso).mark_bar().encode(
-    x='Grupo de Cursos (Graduação)', y='Quantidade de Inscrições')
+    x=at.X('Grupo de Cursos (Graduação):N',
+           sort=at.SortField(field='Quantidade de Inscrições',
+                             order='descending')),
+    y='Quantidade de Inscrições:Q')
 
 #Sidebar com os filtros
 with st.sidebar:
